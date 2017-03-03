3a7ebb5b4554fdeea0bf323da965946261306b6a
lshapz <laurashapiro01@gmail.com>
2017-03-02 23:08:20 -0500

    gridmaker algorithm streamlined like whoa

--- src/components/gridmaker.js
+++ src/components/gridmaker.js
@@ -5,154 +5,70 @@ function gridMaker (height, width){
   for(var i=0; i<height; i++) {
       emptyArray.push(new Array(width).fill(null));
   }
-
-  if (height * width < 250) {
-      Array.from(Array(20)).forEach((x, i)=> {
+  if (height * width < 100) {
+    Array.from(Array(10)).forEach((x, i)=>{
       let row =  Math.floor((Math.random() * height))
       let column =  Math.floor((Math.random() * width))
       emptyArray[row][column] = "mine"
     })
-
   }
-
+  else if (height * width < 250 && height * width >= 100 ) {
+    Array.from(Array(20)).forEach((x, i)=>{
+      let row =  Math.floor((Math.random() * height))
+      let column =  Math.floor((Math.random() * width))
+      emptyArray[row][column] = "mine"
+    })
+  }
   else if (height * width > 250 && height * width < 400) {
-      Array.from(Array(40)).forEach((x, i)=> {
+    Array.from(Array(40)).forEach((x, i)=>{
       let row =  Math.floor((Math.random() * height))
       let column =  Math.floor((Math.random() * width))
       emptyArray[row][column] = "mine"
     })
   }
   else {
-      Array.from(Array(60)).forEach((x, i)=>{
+    Array.from(Array(60)).forEach((x, i)=>{
       let row =  Math.floor((Math.random() * height))
       let column =  Math.floor((Math.random() * width))
       emptyArray[row][column] = "mine"
     })
   }
 
-  let numbersArray =  emptyArray.map((line, lineIndex)=>{
-    return line.map((cell, cellIndex)=>{
+  let numbersArray = emptyArray.map((line, row)=>{
+    return line.map((cell, column)=>{
     let mines = 0 
-    // let neighbors = [ 
-    //   [row-1, column-1], [row-1, column], [row-1, column+1], 
-    //   [row, column-1], [row, column+1],
-    //   [row+1, column-1], [row+1, column], [row+1, column+1]
-    // ]
-    if (lineIndex === 0 && cellIndex === 0) {
-         if (emptyArray[lineIndex][cellIndex+1] === "mine")
-          { mines = 1 }
-         if (emptyArray[lineIndex+1][cellIndex] === "mine")
-          { mines = 1 }
-         if (emptyArray[lineIndex+1][cellIndex+1] === "mine")
-          { mines = 1 }
+    let neighbors = [ 
+      [row-1, column-1], [row-1, column], [row-1, column+1], 
+      [row, column-1], [row, column+1],
+      [row+1, column-1], [row+1, column], [row+1, column+1]
+    ]
+    neighbors.forEach((friend)=>{
+      let [friendRow, friendColumn] = friend
+      if ((friendRow >= 0 && friendRow <= height - 1) && (friendColumn >= 0 && friendColumn <= width - 1)) {
+        if (emptyArray[friendRow][friendColumn] === "mine"){
+          mines += 1
+        }
+      }
+    })
+    if (cell === "mine") {
+      mines = "mine"
     }
-    else if (lineIndex === height-1 && cellIndex === width-1) {
-         if (emptyArray[lineIndex-1][cellIndex-1] === "mine")
-          { mines = 1 }
-         if (emptyArray[lineIndex-1][cellIndex] === "mine")
-          { mines = 1 }
-         if (emptyArray[lineIndex][cellIndex-1] === "mine")
-          { mines = 1 }
-    }
-    else if (lineIndex === 0 && cellIndex === width-1) {
-         if (emptyArray[lineIndex][cellIndex-1] === "mine")
-          { mines = 1 }
-         if (emptyArray[lineIndex+1][cellIndex] === "mine")
-          { mines = 1 }
-         if (emptyArray[lineIndex+1][cellIndex-1] === "mine")
-          { mines = 1 }
-    }
-    else if (lineIndex === height-1 && cellIndex === 0) {
-         if (emptyArray[lineIndex-1][cellIndex] === "mine")
-          { mines = 1 }
-         if (emptyArray[lineIndex-1][cellIndex+1] === "mine")
-          { mines = 1 }
-         if (emptyArray[lineIndex][cellIndex+1] === "mine")
-          { mines = 1 }
-    }
-    else if (lineIndex === 0 && cellIndex !== 0) {
-       if (emptyArray[lineIndex][cellIndex-1] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex][cellIndex+1] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex+1][cellIndex-1] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex+1][cellIndex] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex+1][cellIndex+1] === "mine")
-        { mines += 1 }
-    }
-    else if (cellIndex === 0  && lineIndex !== 0) {
-       if (emptyArray[lineIndex-1][cellIndex] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex-1][cellIndex+1] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex][cellIndex+1] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex+1][cellIndex] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex+1][cellIndex+1] === "mine")
-        { mines += 1 }
-    }
-    else if (lineIndex === height-1){
-       if (emptyArray[lineIndex][cellIndex-1] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex][cellIndex+1] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex-1][cellIndex-1] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex-1][cellIndex] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex-1][cellIndex+1] === "mine")
-        { mines += 1 }
-    }
-    else if (cellIndex === width-1) {
-       if (emptyArray[lineIndex-1][cellIndex] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex-1][cellIndex-1] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex][cellIndex-1] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex+1][cellIndex] === "mine")
-        { mines += 1 }
-       if (emptyArray[lineIndex+1][cellIndex-1] === "mine")
-        { mines += 1 } 
-    }    
-    else {
-     if (emptyArray[lineIndex-1][cellIndex-1] === "mine")
-      { mines += 1 }
-     if (emptyArray[lineIndex-1][cellIndex] === "mine")
-      { mines += 1 }
-     if (emptyArray[lineIndex-1][cellIndex+1] === "mine")
-      { mines += 1 }
-     if (emptyArray[lineIndex][cellIndex-1] === "mine")
-      { mines += 1 }
-     if (emptyArray[lineIndex][cellIndex+1] === "mine")
-      { mines += 1 }
-     if (emptyArray[lineIndex+1][cellIndex-1] === "mine")
-      { mines += 1 }
-     if (emptyArray[lineIndex+1][cellIndex] === "mine")
-      { mines += 1 }
-     if (emptyArray[lineIndex+1][cellIndex+1] === "mine")
-      { mines += 1 }
-    }
-
-    if (cell === "mine")
-        {mines = "mine"}
-  
     return mines
     })
-
   })
 
-  let objectsArray = numbersArray.map((line, indexA)=>{
-    return line.map((cell, indexB)=>{
-      return {row: indexA, column: indexB, flag: false, mine: cell === 'mine' ? true : false, clicked: false, text: cell }
+  let objectsArray = numbersArray.map((line, rowIndex)=>{
+    return line.map((cell, columnIndex)=>{
+      return {row: rowIndex, column: columnIndex, flag: false, mine: cell === 'mine' ? true : false, clicked: false, text: cell }
     })
   })
 
-  let mineCount = objectsArray.reduce((a,b)=>{return a.concat(b)}).filter(square=>{return square.mine === true})
-
+  let mineCount = objectsArray.reduce((lineA,lineB)=>{
+      return lineA.concat(lineB)
+    }).filter(square=>{
+      return square.mine === true
+    })
+    
   return [objectsArray, mineCount.length]
   
 }
